21fb5ea0a7b45a2bb998618372a096b3d2999797
opajkovic <oljaolja170@gmail.com>
2023-07-27 13:24:35 +0200

    validate editProduct

--- src/components/pages/EditProduct.jsx
+++ src/components/pages/EditProduct.jsx
@@ -1,83 +1,113 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import classes from './EditProduct.module.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ProductContext } from '../../store/product-context';
+import { Formik } from 'formik';
 
 const EditProduct = () => {
   const id = useParams().id;
   const navigate = useNavigate();
-  const { items,setItems } = useContext(ProductContext);
+  const { items, setItems } = useContext(ProductContext);
   const item = items.find((i) => i.id === Number(id));
-  const [itemUpdate, setItemUpdate] = useState({
-    id: id,
-    title: item.title,
-    description:item.description ,
-    price: item.price,
-    thumbnail: item.thumbnail,
-    images: item.images,  
-  });
-
- 
-const changeHandler = (e) => {
-    const { name, value } = e.target;
-    
-    setItemUpdate({ ...itemUpdate, [name]: value })
-   
-}
-  const submitHandler = (e) => {
-   e.preventDefault();
-   setItems(prevItems => {
-    return prevItems.map( p => { 
-      if (p.id == itemUpdate.id) {
-        return { ...p, title: itemUpdate.title, description: itemUpdate.description, price:itemUpdate.price}
-      } else {
-        return p;
-        
-      }
-    })
-  })
-    navigate('/');
-}
 
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
-      <div className={classes.controlGroup}>
-        <div className={classes.formControl}>
-          <label htmlFor='title'>Title</label>
-          <input
-            type='text'
-            id='title'
-            name="title"
-            value={itemUpdate.title}
-            onChange={(e) => changeHandler(e)}
-          />
-        </div>
-         <div className={classes.formControl}>
-          <label htmlFor='description'>Description</label>
-          <input
-            type='text'
-            id='description'
-            name='desciption'
-            value={itemUpdate.description}        
-              onChange={(e) => changeHandler(e)}
-          />
-        </div>  
-      <div className={classes.formControl}>
-        <label htmlFor='price'>Price</label>
-        <input
-          type='text'
-          id='price'
-          name='price'
-          value={itemUpdate.price}
-          onChange={(e) => changeHandler(e)}
-        />
-      </div>
-    </div>
-      <div className={classes.formActions}>
-        <button >Submit</button>
-      </div>
+    <>
+      <Formik initialValues={{
+        id: item.id,
+        title: item.title,
+        description: item.description,
+        price: item.price
+      }}
+        onSubmit={(values) => {
+          const { id, title, description, price } = values;
+          setItems(prevItems => {
+            return prevItems.map(p => {
+              if (p.id === +id) {
+                return { ...p, title: title, description: description, price: price }
+              } else {
+                return p;
+              }
+            })
+          })
+          navigate('/');
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.title) { errors.title = "Please enter a title"; }
+          else if (values.title.length < 3) { errors.title = "Title is to short"; }
+          else if (values.title.length > 20) { errors.title = "Title is to long"; }
+          else if (!/^[A-Za-z0-9 ]*$/i.test(values.title)) {
+            errors.title = "Use only letters or numbers";
+          }
+          if (!values.description) { errors.description = "Please enter a description"; }
+          if (!values.price) { errors.price = "Please enter a price"; }
+          else if (!/^-?\d+(\.\d+)?$/.test(values.price)) { errors.price = "Use only a numbers "; }
+          return (errors);
+        }}
+      >
+        {({
+          values,
+          handleSubmit,
+          isValid,
+          isSubmitting,
+          handleBlur,
+          handleChange,
+          touched,
+          errors
+        }) => (
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <div className={classes.controlGroup}>
+              <div className={classes.formControl}>
+                <label htmlFor='title'>Title</label>
+                <input
+                  type='text'
+                  id='title'
+                  name="title"
+                  value={values.title}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={touched.title && errors.title ? "error" : null}
+                />
+                {touched.title && errors.title
+                  ? (<div className="error-message">{errors.title}</div>) : null}
+              </div>
+              <div className={classes.formControl}>
+                <label htmlFor='description'>Description</label>
+                <input
+                  type='text'
+                  id='description'
+                  name='description'
+                  value={values.description}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={touched.description && errors.description ? "error" : null}
+                />
+                {touched.description && errors.description
+                  ? (<div className="error-message">{errors.description}</div>) : null}
+              </div>
+              <div className={classes.formControl}>
+                <label htmlFor='price'>Price</label>
+                <input
+                  type='text'
+                  id='price'
+                  name='price'
+                  value={values.price}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={touched.price && errors.price ? "error" : null}
+                />
+                {touched.price && errors.price
+                  ? (<div className="error-message">{errors.price}</div>) : null}
+              </div>
+            </div>
+            <div className={classes.formActions}>
+              <button type="submit" disabled={!(isValid) || isSubmitting}>Submit</button>
+            </div>
 
-    </form>
+          </form>
+        )}
+      </Formik>
+    </>
   )
 }
 
